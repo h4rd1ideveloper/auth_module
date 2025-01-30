8e5fbdab06e9d2aefcdb5e51593c066cf7819a46
Yan Policarpo <h4rd1i@gmail.com>
2025-01-30 10:09:46 -0300

    test: rota / para teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
